9f8f62d15cb01d6150ecf6f9a2e83147ca9b2673
WangKe-117 <bloodengine117@gmail.com>
2025-03-12 15:22:20 +0800

    Regular Commit

--- src/main/java/GroupAnagrams.java
+++ src/main/java/GroupAnagrams.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 public class GroupAnagrams {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        String[] strs = new String[size];
+        scanner.nextLine();
+        for (int i = 0; i < size; i++) {
+            strs[i] = scanner.nextLine();
+        }
+        List<List<String>> result = solution(strs);
+        System.out.println(result);
+    }
 
+    public static List<List<String>> solution(String[] strs){
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            if (map.containsKey(sortedStr)){
+                map.get(sortedStr).add(str);
+            }
+            else {
+                ArrayList<String> array = new ArrayList<>();
+                array.add(str);
+                map.put(sortedStr, array);
+            }
+        }
+        return new ArrayList<>(map.values());
     }
 }
