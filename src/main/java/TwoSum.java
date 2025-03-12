5543d8c721d05e5063b8891197c10d296fec7dc0
WangKe-117 <501277312@qq.com>
2025-03-12 15:22:20 +0800

    Regular Commit

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -22,7 +22,7 @@ public class TwoSum {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++){
             int diff = target - nums[i];
-            if(map.containsKey(diff)){
+            if (map.containsKey(diff)){
                 int r2 = map.get(diff);
                 return new int[]{i, r2};
             }
