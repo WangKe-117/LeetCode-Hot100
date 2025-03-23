b031fe04f1546faedbcb3ffa5d54ee4686267eed
WangKe-117 <bloodengine117@gmail.com>
2025-03-23 14:55:12 +0800

    Regular Commit

--- src/main/java/RotateArray.java
+++ src/main/java/RotateArray.java
@@ -17,20 +17,40 @@ public class RotateArray {
         System.out.println(Arrays.toString(nums));
     }
 
+//    public static void solution(int[] nums, int k) {
+//        int len = nums.length;
+//        int[] copyNum = new int[len];
+//        if (k % len != 0) {
+//            k = k % len;
+//            for (int i = 0; i < len; i++) {
+//                copyNum[i] = nums[i];
+//            }
+//            for (int i = 0; i < k; i++) {
+//                nums[i] = copyNum[i+len-k];
+//            }
+//            for (int i = k; i < len; i++) {
+//                nums[i] = copyNum[i-k];
+//            }
+//        }
+//    }
+
     public static void solution(int[] nums, int k) {
-        int len = nums.length;
-        int[] copyNum = new int[len];
-        if (k % len != 0) {
-            k = k % len;
-            for (int i = 0; i < len; i++) {
-                copyNum[i] = nums[i];
-            }
-            for (int i = 0; i < k; i++) {
-                nums[i] = copyNum[i+len-k];
-            }
-            for (int i = k; i < len; i++) {
-                nums[i] = copyNum[i-k];
-            }
+        if (k % nums.length != 0) {
+            k = k % nums.length;
+            reverse(nums, 0, nums.length - 1);
+            reverse(nums, 0, k - 1);
+            reverse(nums, k, nums.length - 1);
+        }
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        int left = start, right = end;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
     }
 }
